add1cf33ed79d38e197b3769cf81758e1df2037a
Lethary <boudetk@saintjo.org>
2025-09-25 20:45:52 +0200

    Test push

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>StuddyBuddy</title>
 </head>
+<header>
+    <h1>Welcome to StuddyBuddy</h1>
+</header>
 <body>
-    
+
 </body>
 </html>
