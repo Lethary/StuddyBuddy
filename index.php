01a8544c88278923179bcaf8c17e7a2a31dcacdd
Lethary <boudetk@saintjo.org>
2025-10-01 13:30:10 +0200

    Index

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>StuddyBuddy</title>
 </head>
+
 <body>
-    <p>TEst de deploy</p>
-    <p>2</p>
-    <p>3</p>
-    <p>4</p>
-    <p>5</p>
+    <header>
+
+    </header>
+
+    <main>
+
+    </main>
+    <footer>
+        
+    </footer>
 </body>
+
 </html>
