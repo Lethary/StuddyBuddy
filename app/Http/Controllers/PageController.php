517fceeda06772140d2637a9f8427116188a7d76
Lethary <boudetk@saintjo.org>
2025-10-08 15:42:27 +0200

    Test

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function accueil()
+    {
+        return view('accueil');
+    }
 }
---
+++ resources/views/accueil.blade.php
@@ -0,0 +1,1 @@
+<h1>Bienvenue sur StudyBuddy 🚀</h1>
