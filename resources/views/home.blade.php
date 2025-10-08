573069441bdcef7b867e2ebadad92e31ac6597cd
Lethary <boudetk@saintjo.org>
2025-10-08 15:38:37 +0200

    .

--- resources/views/home.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Accueil')
-
-@section('content')
-<h1>Bienvenue sur StuddyBuddy</h1>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
 
-Route::get('/', [PageController::class, 'home'])->name('home');
-
-Route::get('/welcome', [PageController::class, 'welcome'])->name('welcome');
-
+Route::get('/', function () {
+    return view('welcome');
+});
